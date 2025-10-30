46e57dbdbaad9be27229c51473b91fe8840c6098
MikulDev <gamer3mc@gmail.com>
2025-10-29 23:14:36 -0400

    Scale player stats logarithmically

--- src/main/java/com/momosoftworks/scalingmobs/util/MathHelper.java
+++ src/main/java/com/momosoftworks/scalingmobs/util/MathHelper.java
@@ -1,5 +1,7 @@
 package com.momosoftworks.scalingmobs.util;
 
+import net.minecraft.util.Mth;
+
 public class MathHelper
 {
     public static double blend(double blendFrom, double blendTo, double factor, double rangeMin, double rangeMax)
@@ -9,5 +11,15 @@ public class MathHelper
         if (factor <= rangeMin) return blendFrom;
         if (factor >= rangeMax) return blendTo;
         return (blendTo - blendFrom) / (rangeMax - rangeMin) * (factor - rangeMin) + blendFrom;
+    }
+
+    public static double blendLog(double blendFrom, double blendTo, double factor, double rangeMin, double rangeMax, double intensity)
+    {
+        factor = Mth.clamp(factor, rangeMin, rangeMax);
+
+        double normalizedFactor = (factor - rangeMin) / (rangeMax - rangeMin);
+        double logFactor = Math.log(intensity * normalizedFactor + 1) / Math.log(intensity + 1);
+
+        return blendFrom + (blendTo - blendFrom) * logFactor;
     }
 }
